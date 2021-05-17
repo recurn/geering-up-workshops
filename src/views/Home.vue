ed756207de6ecd3c20858ed24b7f879d19f3fa3a
recurn <61036489+recurn@users.noreply.github.com>
2021-05-17 16:34:41 -0700

    firebase init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Reid is cool.</h1>
+    <p>And this is all I'm going to do today.</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
